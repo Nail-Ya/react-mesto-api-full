25774e1809b0a480bb18cca042b1273d571c36e5
Nail-Ya <nailgalimov9@gmail.com>
2020-10-21 12:50:33 +0500

    fix: authorization

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'http://localhost:3000',
 }));
 
 // подключаемся к серверу mongo
