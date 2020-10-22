ed64ff0a6468912e9863ce53022dee9894c196ee
Nail-Ya <nailgalimov9@gmail.com>
2020-10-22 20:50:35 +0500

    fix: change cors options

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ require('dotenv').config();
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors({
-  origin: 'https://gss.students.nomoreparties.xyz',
-}));
+app.use(cors());
 
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mydb', {
